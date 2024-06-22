3947d17bfe5b71d9a4c160abb26198795d9b34b2
nikoyangar <nikoyangar@gmail.com>
2024-06-23 00:16:13 +0400

    temp commit

---
+++ Lesson_Ref_Out_Parameters/005_Out_Parameters/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _005_Out_Parameters
+{
+    //ref and out parameters provide ways to pass variables by reference to methods, allowing them to modify the original values
+    //Remember that ref parameters require the variable to be initialized before passing it to the method, while out parameters don't.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            Console.WriteLine("Before: " + number1);
+            ModifyNumber(number1);
+            Console.WriteLine("After: " + number1);
+
+            int number;// = 10;
+            //Console.WriteLine("Before: " + number);
+            ModifyNumber(out number);
+            Console.WriteLine("After: " + number);
+        }
+
+        static void ModifyNumber(int num)
+        {
+            ++num;
+        }
+
+        static void ModifyNumber(out int num)
+        {
+            //Console.WriteLine("Before: " + num);
+            num = 0;
+            ++num;
+        }
+
+        static void ModifyNumber(int num, out int num1)
+        {
+            num1 = 0;
+            ++num;
+            ++num1;
+        }
+
+        static void ModifyNumber(out int num, out int num1, out int num2, int num3)
+        {
+            num = 0;
+            num1 = 0;
+            num2 = 0;
+        }
+    }
+}
---
+++ Lesson_Ref_Out_Parameters/006_Out_Parameters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _004_Out_Parameters
+{
+    //As a parameter modifier, which lets you pass an argument to a method by reference rather than by value.
+    //In generic type parameter declarations for interfaces and delegates, which specifies that a type parameter is covariant.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radiusValue = 3.92781;
+            //Calculate the circumference and area of a circle, returning the results to Main().
+            CalculateCircumferenceAndArea(radiusValue, out double circumferenceResult, out var areaResult);
+
+            Console.WriteLine($"Circumference of a circle with a radius of {radiusValue} is {circumferenceResult}.");
+            Console.WriteLine($"Are of a circle with a radius of {radiusValue} is {areaResult}.");
+            Console.ReadLine();
+        }
+        //The calculation worker method.                                           длина окружности
+        public static void CalculateCircumferenceAndArea(double radius, out double circumference, out double area)
+        {
+            circumference = 2 * Math.PI * radius;
+            area = Math.PI * (radius * radius);
+        }
+    }
+}
