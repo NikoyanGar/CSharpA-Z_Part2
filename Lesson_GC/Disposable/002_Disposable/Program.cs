af30b5648e6fdc0a36c89278ff8a4690ea680bae
nikoyangar <nikoyangar@gmail.com>
2024-07-22 23:06:24 +0400

    add GC examples

---
+++ Lesson_GC/Disposable/002_Disposable/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _002_Disposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ex = new Example();
+
+            try
+            {
+                ex.SayHello();
+            }
+            finally
+            {
+                if (ex is IDisposable && ex != null)
+                {
+                    ex.Dispose();
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+
+    public class Example : IDisposable
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose called");
+        }
+    }
+}
