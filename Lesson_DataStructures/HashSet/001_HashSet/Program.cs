8fad6777aee3727d08d55532525b4fdfe3395e2f
nikoyangar <nikoyangar@gmail.com>
2024-07-22 23:13:51 +0400

    add hashset examples

---
+++ Lesson_DataStructures/HashSet/001_HashSet/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _001_HashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                set.Add(i);
+            }
+        }
+    }
+}
