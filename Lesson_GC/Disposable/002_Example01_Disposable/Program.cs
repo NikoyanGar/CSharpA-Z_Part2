f64f5c7f5905fd3afc9df452ff8e5825c8305727
Garik Nikoyan <garik.nikoyan@highwaybaas.com>
2024-09-17 17:14:42 +0400

    start new examples on Idisposample with test connections

---
+++ Lesson_GC/Disposable/002_Example01_Disposable/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _002_Example01_Disposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePathfullPath = "C:\\Users\\garik.nikoyan\\source\\repos\\CSharpA-Z_Part2\\Lesson_GC\\Disposable\\002_Example01_Disposable\\file.txt";
+
+            //var filePathfullPath1 = @"C:\Users\garik.nikoyan\source\repos\CSharpA-Z_Part2\Lesson_GC\Disposable\002_Example01_Disposable\file.txt";
+
+            using (var file = File.OpenText(filePathfullPath))
+            {
+                var fileContent = file.ReadToEnd();
+            }
+            // var fileContent = File.ReadAllText(filePathfullPath);
+            // equivalent code
+            //var file = File.OpenText(filePathfullPath);
+            //try
+            //{
+            //    var fileContent = file.ReadToEnd();
+            //}
+            //finally
+            //{
+            //    file.Dispose();
+            //}
+        }
+    }
+}
